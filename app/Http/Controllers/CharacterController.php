f3daa453ba16df97ef0d6ede9bef3ea013efc5d4
LucasLimaAZ <lucasdelimamonteiro@gmail.com>
2020-10-23 17:36:02 -0300

    first commit

---
+++ app/Http/Controllers/CharacterController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Character;
+
+class CharacterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json(Character::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $result = Character::create($request->all());
+        return response()->json($result);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::group([
+    'prefix' => 'auth'
+], function () {
+    Route::post('login', 'UsersController@login');
+    Route::post('signup', 'UsersController@signup');
+
+    Route::group([
+        'middleware' => 'auth:api'
+    ], function() {
+        Route::get('logout', 'UsersController@logout');
+        Route::get('user', 'UsersController@user');
+    });
+    
+});
+
+Route::group([
+    'middleware' => 'auth:api'
+], function() {
+    Route::resource('characters', 'CharacterController');
+}); 
