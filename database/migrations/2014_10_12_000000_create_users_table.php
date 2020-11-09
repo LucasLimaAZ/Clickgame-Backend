04fc89f4b51b59f74d95fcec943d8e115c0910ee
Lucas de Lima Monteiro <lucasdelimamonteiro@gmail.com>
2020-11-08 23:44:49 -0300

    new default user generated at migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class CreateUsersTable extends Migration
 {
@@ -24,6 +26,14 @@ class CreateUsersTable extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
+
+        DB::table('users')->insert(
+            array(
+                'name' => 'admin',
+                'email' => 'lucasdelimamonteiro@gmail.com',
+                'password' => Hash::make('nuclear00123')
+            )
+        );
     }
 
     /**
